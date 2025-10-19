c4bdd667cc65cd7f42775a2f00ee06dd0d34fa11
sunwoo1524 <maengseonu80@gmail.com>
2025-10-19 13:04:36 +0900

    fix not recording bug

--- audio-preprocessing.py
+++ audio-preprocessing.py
@@ -4,37 +4,57 @@ import numpy as np
 import sounddevice as sd
 import threading
 
-# mic: TODO
 sr = 22050
 recorded_data = []
 is_recording = False
 
-def record_audio():
-    global recorded_data, is_recording
+# def record_audio():
+#     global recorded_data, is_recording
     
-    def callback(indata, frames, time, status):
-        if status:
-            print(f"상태: {status}")
-        if is_recording:
-            recorded_data.append(indata.copy())
+#     def callback(indata, frames, time, status):
+#         if status:
+#             print(f"상태: {status}")
+#         if is_recording:
+#             recorded_data.append(indata.copy())
     
-    with sd.InputStream(samplerate=sr, channels=1, callback=callback):
-        while is_recording:
-            sd.sleep(100)
+#     with sd.InputStream(samplerate=sr, channels=1, callback=callback):
+#         while is_recording:
+#             sd.sleep(100)
 
-print("녹음 중...")
-print("엔터 키로 녹음 종료")
+# print("녹음 중...")
+# print("엔터 키로 녹음 종료")
 
-is_recording = True
+# is_recording = True
+# recorded_data = []
+
+# record_thread = threading.Thread(target=record_audio)
+# record_thread.start()
+
+# is_recording = False
+# record_thread.join()
+
+# print("녹음 종료")
+
+
+
+print("Enter 키를 누르면 녹음이 종료됩니다.")
+
 recorded_data = []
 
-record_thread = threading.Thread(target=record_audio)
-record_thread.start()
+def callback(indata, frames, time, status):
+    if status:
+        print(f"상태: {status}")
+    recorded_data.append(indata.copy())
 
-is_recording = False
-record_thread.join()
+# 스트림 생성 (논블로킹 모드)
+stream = sd.InputStream(samplerate=sr, channels=1, callback=callback, blocksize=1024)
 
-print("녹음 종료")
+stream.start()
+input()  # Enter 입력 대기
+stream.stop()
+stream.close()
+
+print("녹음이 종료되었습니다!")
 
 audio_data = np.concatenate(recorded_data, axis=0).flatten()
 
@@ -56,7 +76,7 @@ mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
 import matplotlib.pyplot as plt
 
 plt.subplot(2, 1, 1)
-librosa.display.waveshow(y, sr=sr)
+librosa.display.waveshow(audio_data, sr=sr)
 plt.title('Audio Waveform')
 plt.xlabel('Time (sec)')
 plt.ylabel('Amplitude')
