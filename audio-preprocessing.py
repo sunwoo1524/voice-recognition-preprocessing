f4246bd0b5cfcc4f4bd49755ff6bbd9f8aacf81b
sunwoo1524 <maengseonu80@gmail.com>
2025-10-19 13:29:01 +0900

    set recording time

--- audio-preprocessing.py
+++ audio-preprocessing.py
@@ -2,29 +2,38 @@ import librosa
 import librosa.display
 import numpy as np
 import sounddevice as sd
-import threading
 
 sr = 22050
 
-print("녹음이 시작되었습니다.\nEnter 키를 눌러 녹음을 종료하세요.")
+# print("녹음이 시작되었습니다.\nEnter 키를 눌러 녹음을 종료하세요.")
 
-recorded_data = []
+# recorded_data = []
 
-def callback(indata, frames, time, status):
-    if status:
-        print(f"상태: {status}")
-    recorded_data.append(indata.copy())
+# def callback(indata, frames, time, status):
+#     if status:
+#         print(f"상태: {status}")
+#     recorded_data.append(indata.copy())
 
-stream = sd.InputStream(samplerate=sr, channels=1, callback=callback, blocksize=1024)
+# stream = sd.InputStream(samplerate=sr, channels=1, callback=callback, blocksize=1024)
 
-stream.start()
-input()
-stream.stop()
-stream.close()
+# stream.start()
+# input()
+# stream.stop()
+# stream.close()
 
+# print("녹음이 종료되었습니다!")
+
+# audio_data = np.concatenate(recorded_data, axis=0).flatten()
+
+DURATION = 5 # record for 5 sec
+
+print(f"녹음 중...")
+recording = sd.rec(int(DURATION * sr), samplerate=sr, channels=1)
+sd.wait()
 print("녹음이 종료되었습니다!")
 
-audio_data = np.concatenate(recorded_data, axis=0).flatten()
+# convert to 1d array
+audio_data = recording.flatten()
 
 # optimum setting values for voice recognition (by Claude)
 mel_spectrogram = librosa.feature.melspectrogram(
@@ -38,32 +47,33 @@ mel_spectrogram = librosa.feature.melspectrogram(
 
 mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
 
-# print(mel_spectrogram_db.shape)
+print(mel_spectrogram_db.shape)
+print(mel_spectrogram_db)
 
 # vislualize
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-plt.subplot(2, 1, 1)
-librosa.display.waveshow(audio_data, sr=sr)
-plt.title('Audio Waveform')
-plt.xlabel('Time (sec)')
-plt.ylabel('Amplitude')
+# plt.subplot(2, 1, 1)
+# librosa.display.waveshow(audio_data, sr=sr)
+# plt.title('Audio Waveform')
+# plt.xlabel('Time (sec)')
+# plt.ylabel('Amplitude')
 
-plt.subplot(2, 1, 2)
-img = librosa.display.specshow(
-    mel_spectrogram_db,
-    sr=sr,
-    hop_length=512,
-    x_axis='time',
-    y_axis='mel',
-    fmax=8000,
-    cmap='viridis'
-)
-plt.colorbar(img, format='%+2.0f dB')
-plt.title('Mel Spectrogram')
-plt.xlabel('Time (sec)')
-plt.ylabel('frequency (Hz)')
+# plt.subplot(2, 1, 2)
+# img = librosa.display.specshow(
+#     mel_spectrogram_db,
+#     sr=sr,
+#     hop_length=512,
+#     x_axis='time',
+#     y_axis='mel',
+#     fmax=8000,
+#     cmap='viridis'
+# )
+# plt.colorbar(img, format='%+2.0f dB')
+# plt.title('Mel Spectrogram')
+# plt.xlabel('Time (sec)')
+# plt.ylabel('frequency (Hz)')
 
-plt.tight_layout()
-plt.savefig('mel_spectrogram.png', dpi=300, bbox_inches='tight')
+# plt.tight_layout()
+# plt.savefig('mel_spectrogram.png', dpi=300, bbox_inches='tight')
 # plt.show()
